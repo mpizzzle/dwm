410971d855d81146ce85cdd2de78abfa5d3d79df
mpizzzle <m@michaelpercival.xyz>
2021-08-29 10:19:22 +0100

    moved wallpapers directory

--- config.def.h
+++ config.def.h
@@ -117,7 +117,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                       XK_y,      spawn,          SHCMD("wallpapers.sh ~/wallpapers &") },
+	{ MODKEY,                       XK_y,      spawn,          SHCMD("wallpapers.sh ~/documents/wallpapers &") },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -128,7 +128,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
  	{ MODKEY,                       XK_n,      togglealttag,   {0} },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          SHCMD("lock.sh &") },
-	{ MODKEY,                       XK_Print,      spawn,      SHCMD("scrot") },
+	{ MODKEY,                       XK_Print,  spawn,          SHCMD("scrot -u") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
